259b88de8d24ff9e76286e997a16086a81fd6f25
pieceofr <komimi.p@gmail.com>
2018-12-15 21:02:10 +0800

    modify message

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nlopes/slack"
@@ -10,7 +11,7 @@ import (
 
 var chanName = []string{"公益"}
 var chanCode = []string{"CEVRD231V"}
-var slackToken = "xoxp-166092624144-167490026647-504798328002-c11e5b02a0790bdbe4a57a5fe610bb4f"
+var slackToken = "xoxp-166092624144-167490026647-504760819587-cc8a485e1f50a2bf078a83fb2cec1dd1"
 
 type slackChan struct {
 	Name string
@@ -24,9 +25,18 @@ func main() {
 
 func runHttpServ(port string) {
 	route := gin.Default()
-	route.Run(":" + port)
-	route.POST("/sendMessage", sendMessage)
-	route.Run() // listen and serve on 0.0.0.0:8080
+	route.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "test ok")
+	})
+
+	route.GET("/sendMessage", sendMessage)
+	route.Run(":" + port) // listen and serve on 0.0.0.0:8080
+}
+
+func test(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": "ok",
+	})
 }
 
 func sendMessage(c *gin.Context) {
@@ -49,7 +59,22 @@ func identifyChan(c string) slackChan {
 func slackSendMessage(channel slackChan, msg string) {
 	slackapi := slack.New(slackToken)
 	log.Println("slack send to " + channel.Name + ":" + msg)
-	channelID, timestamp, err := slackapi.PostMessage(channel.Code, slack.MsgOptionText(msg, false), slack.MsgOptionAttachments(slack.Attachment{}))
+	attachment := slack.Attachment{
+		Pretext: "",
+		Text:    msg,
+		// Uncomment the following part to send a field too
+		/*
+			Fields: []slack.AttachmentField{
+				slack.AttachmentField{
+					Title: "a",
+					Value: "no",
+				},
+			},
+		*/
+	}
+	channelID, timestamp, err := slackapi.PostMessage(channel.Code,
+		slack.MsgOptionText(msg, false),
+		slack.MsgOptionAttachments(attachment))
 	//_, _, err := slackapi.PostMessage(channel.Name, msg, slack.PostMessageParameters{})
 	if err != nil {
 		fmt.Printf("%s\n", err)
